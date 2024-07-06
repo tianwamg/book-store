614fda1ecbd49a48dc1add925c9052b512a20ab4
wangpanpan@leoao.com <leke@2015>
2024-07-06 16:19:29 +0800

    发布

--- src/main/java/com/cn/controller/BookPushController.java
+++ src/main/java/com/cn/controller/BookPushController.java
@@ -1,10 +1,13 @@
 package com.cn.controller;
 
+import com.cn.dto.PushTaskDto;
 import com.cn.request.CommonRequest;
 import com.cn.response.ResultResponse;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +24,8 @@ public class BookPushController {
     @Autowired
     private RedisTemplate redisTemplate;
 
-    public ResultResponse push(CommonRequest request){
+    @PostMapping("/send/task")
+    public ResultResponse push(@RequestBody CommonRequest<PushTaskDto> request){
         //获取推送信息，拿取缓存session，推送至mq
         String sessionKey = redisTemplate.opsForValue().get("taobao_"+request.getUserId()).toString();
         return null;
--- src/main/java/com/cn/controller/TaobaoPushController.java
+++ src/main/java/com/cn/controller/TaobaoPushController.java
@@ -4,30 +4,38 @@ import com.cn.dto.PushTaskDto;
 import com.cn.request.CommonRequest;
 import com.cn.response.ResultResponse;
 import com.cn.tbapi.SingletonClient;
+import com.cn.tbapi.TaobaoService;
 import com.taobao.api.DefaultTaobaoClient;
 import com.taobao.api.TaobaoClient;
+import com.taobao.api.domain.ItemCat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
  * 淘宝商品管理
  */
 @RestController
-@RequestMapping("sys/taobao")
+@RequestMapping("/sys/taobao")
 public class TaobaoPushController {
 
     @Autowired
     RedisTemplate redisTemplate;
 
+    @Autowired
+    TaobaoService taobaoService;
+
     /**
      * 保存用户session
      * @return
      */
-    public ResultResponse saveSessionKey(CommonRequest request){
+    public ResultResponse saveSessionKey(@RequestBody CommonRequest request){
         redisTemplate.opsForValue().set("taobao_"+request.getUserId(),request.getRequestData().toString(),7, TimeUnit.DAYS);
         return ResultResponse.success("true");
     }
@@ -36,7 +44,7 @@ public class TaobaoPushController {
      * 获取用户session
      * @return
      */
-    public ResultResponse<String> getSessionKey(CommonRequest request){
+    public ResultResponse<String> getSessionKey(@RequestBody CommonRequest request){
         String sessionKey = redisTemplate.opsForValue().get("taobao_"+request.getUserId()).toString();
         return ResultResponse.success(sessionKey);
     }
@@ -46,25 +54,27 @@ public class TaobaoPushController {
      * 获取分类模版
      * @return
      */
-    public ResultResponse getCatTemplate(CommonRequest request){
-        TaobaoClient client = SingletonClient.INSTANCE.getClient();
-        return null;
+    @PostMapping("/temp/cat")
+    public ResultResponse<List<ItemCat>> getCatTemplate(@RequestBody CommonRequest request){
+
+        return ResultResponse.success(taobaoService.getCatTemplate());
+
     }
 
     /**
      * 获取用户自定义模版
      */
-    public ResultResponse getUserTemplate(CommonRequest request){
-        TaobaoClient client = SingletonClient.INSTANCE.getClient();
-        return null;
+    @PostMapping("/seller/cat")
+    public ResultResponse getCatSerller(@RequestBody CommonRequest request){
+        return ResultResponse.success(taobaoService.getSellerCat(request.getUserId()));
     }
 
     /**
      * 获取店铺运费模版
      * @return
      */
-    public ResultResponse getPostFeeTemplate(CommonRequest request){
-        TaobaoClient client = SingletonClient.INSTANCE.getClient();
-        return null;
+    @PostMapping("/temp/fee")
+    public ResultResponse getPostFeeTemplate(@RequestBody CommonRequest request){
+        return ResultResponse.success(taobaoService.getPostFeeTemplate(request.getUserId()));
     }
 }
