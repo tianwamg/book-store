323eb1f046587fb6b92d2ba4018f85e2c0e467f7
wangpanpan@leoao.com <leke@2015>
2024-07-11 23:06:33 +0800

    图书列表

--- src/main/java/com/cn/controller/BookInfoController.java
+++ src/main/java/com/cn/controller/BookInfoController.java
@@ -1,6 +1,14 @@
 package com.cn.controller;
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.cn.domain.BookInfo;
+import com.cn.request.CommonRequest;
+import com.cn.response.ResultResponse;
+import com.cn.service.IBookInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2024-05-03
  */
 @RestController
-@RequestMapping("/book-info")
+@RequestMapping("/book/info")
 public class BookInfoController {
+
+    @Autowired
+    IBookInfoService iBookInfoService;
+
+    @PostMapping("/pagelist")
+    public ResultResponse<Page<BookInfo>> getPageList(@RequestBody CommonRequest<BookInfo> request){
+        return ResultResponse.success(iBookInfoService.getPageList(request));
+    }
 
 }
 
